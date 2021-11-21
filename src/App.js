182449106898162a9c6beacfcd03136a039da4e7
Quiet0o <mikolajklosowski112@gmail.com>
2021-11-21 18:38:38 +0100

    hooks test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
 
-
+  const [clickedButton, setclickedButton] = useState(false);
+  
   return (
     <div className="App">
       <h1>Dupa</h1>
+      <button onClick={()=>setclickedButton(!clickedButton)} >{clickedButton.toString()}</button>
     </div>
   );
 }
