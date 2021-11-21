23790142b78463790387fc06f9e9fc073ea4c0c2
Quiet0o <mikolajklosowski112@gmail.com>
2021-11-21 18:43:41 +0100

    hooks test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { useState } from 'react';
 function App() {
 
   const [clickedButton, setclickedButton] = useState(false);
-  
+  const [number, setnumber] = useState(0)
   return (
     <div className="App">
       <h1>Dupa</h1>
       <button onClick={()=>setclickedButton(!clickedButton)} >{clickedButton.toString()}</button>
+      <button onClick={()=>setnumber(number+1)} >{number}</button>
     </div>
   );
 }
