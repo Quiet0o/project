4837448bc8ff306ee342eb94b2753d7d9693df59
Quiet0o <mikolajklosowski112@gmail.com>
2021-11-23 17:32:24 +0100

    working with firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import './App.css';
 import { useState,useEffect } from 'react';
-import SingIn from "./componets/SignIn"
+import app from './firebase'
+import firebase from 'firebase/compat/app';
 
+const auth = app.auth();
+const SignIn=()=>{
 
+  const SingInWithGoogle =()=>{
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider);
+    }
+
+    const SingInWithFacebook =()=>{
+      const provider = new firebase.auth.FacebookAuthProvider();
+      auth.signInWithPopup(provider)
+    }
+
+    const SingInWithGithub =()=>{
+      const provider = new firebase.auth.GithubAuthProvider();
+      auth.signInWithPopup(provider);
+    }
+
+    return(
+      <div>
+        
+        <button onClick={SingInWithGoogle}>Sing in with Google account</button>
+        <button onClick={SingInWithFacebook}>Sing in with FacebookAuthProvider account</button>
+        <button onClick={SingInWithGithub}>Sing in with Github account</button>
+      </div>
+    )
+}
+const SingOut =()=>{
+  
+  const logout =()=>{
+   return  auth.signOut();
+  }
+
+  return(
+    <button onClick={logout}>Sing out with Google account</button>
+  )
+}
 function App() {
 
   const [clickedButton, setclickedButton] = useState(false);
@@ -22,8 +59,11 @@ function App() {
       <h1>Dupa</h1>
       <button onClick={()=>setclickedButton(!clickedButton)} >{clickedButton.toString()}</button>
       <button onClick={()=>setcount(count+1)} >{count}</button> 
-      <SingIn/>
+      <SignIn/>
+      <p>{auth.currentUser.email}</p>
+      <img src={auth.currentUser.photoURL}/>
       {/* <Register/>  */}
+      <SingOut/>
     </div>
   );
 }
