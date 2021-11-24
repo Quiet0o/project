d2b87fe58e3bc13385bf652388db157a84b8bff8
Quiet0o <mikolajklosowski112@gmail.com>
2021-11-24 22:59:12 +0100

    working on login in

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
 import './App.css';
-import { useState,useEffect } from 'react';
-import app from './firebase'
-import firebase from 'firebase/compat/app';
-
-const auth = app.auth();
-const SignIn=()=>{
-
-  const SingInWithGoogle =()=>{
-      const provider = new firebase.auth.GoogleAuthProvider();
-      auth.signInWithPopup(provider);
-    }
-
-    const SingInWithFacebook =()=>{
-      const provider = new firebase.auth.FacebookAuthProvider();
-      auth.signInWithPopup(provider)
-    }
-
-    const SingInWithGithub =()=>{
-      const provider = new firebase.auth.GithubAuthProvider();
-      auth.signInWithPopup(provider);
-    }
-
-    return(
-      <div>
-        
-        <button onClick={SingInWithGoogle}>Sing in with Google account</button>
-        <button onClick={SingInWithFacebook}>Sing in with FacebookAuthProvider account</button>
-        <button onClick={SingInWithGithub}>Sing in with Github account</button>
-      </div>
-    )
-}
+import SingIn from './componets/SignIn';
+import auth from './firebase'
 const SingOut =()=>{
   
   const logout =()=>{
@@ -42,28 +13,16 @@ const SingOut =()=>{
 }
 function App() {
 
-  const [clickedButton, setclickedButton] = useState(false);
-  const [count, setcount] = useState(0);
-
-  useEffect(() => {
-
-    if(count === 10){
-      console.log("dupa");
-    }
-
-  }, [count])
-
   return (
     <div className="App">
     
-      <h1>Dupa</h1>
-      <button onClick={()=>setclickedButton(!clickedButton)} >{clickedButton.toString()}</button>
-      <button onClick={()=>setcount(count+1)} >{count}</button> 
-      <SignIn/>
-      <p>{auth.currentUser.email}</p>
-      <img src={auth.currentUser.photoURL}/>
-      {/* <Register/>  */}
+      <h1>SUPER SKLEP</h1>
+      <SingIn/>
+      {/* <p>{auth.currentUser.email}</p>
+      <p>{auth.currentUser.displayName}</p>
+      <img src={auth.currentUser.photoURL}/> */}
       <SingOut/>
+      <footer>Nie posiadamy akcyzy na alkohol, poniewarz piwo to nie alkohol 🍻</footer>
     </div>
   );
 }
---
+++ src/componets/Signin.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import firebase from 'firebase/compat/app';
+import app from '../firebase'
+
+
+const auth = app.auth();
+
+const SingIn= ()=>{
+    const SingInWithGoogle =()=>{
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+      }
+  
+      const SingInWithFacebook =()=>{
+        const provider = new firebase.auth.FacebookAuthProvider();
+        auth.signInWithPopup(provider)
+      }
+  
+      const SingInWithGithub =()=>{
+        const provider = new firebase.auth.GithubAuthProvider();
+        auth.signInWithPopup(provider);
+      }
+  
+      return(
+        <div>
+          
+          <button onClick={SingInWithGoogle}>Sing in with Google account</button>
+          <button onClick={SingInWithFacebook}>Sing in with FacebookAuthProvider account</button>
+          <button onClick={SingInWithGithub}>Sing in with Github account</button>
+           
+        </div>
+      )
+
+
+}
+export default SingIn
