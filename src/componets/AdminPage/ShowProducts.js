4cbf68f60cd5acdb324a47dda8127e6b848c8cd6
Quiet0o <mikolajklosowski112@gmail.com>
2021-11-30 17:07:55 +0100

    test

--- src/componets/AdminPage/ShowProducts.js
+++ src/componets/AdminPage/ShowProducts.js
@@ -4,38 +4,6 @@ import  { getDocs,collection }  from "firebase/firestore";
 import Product from './Product';
 const ShowProducts=()=>{
     const [products, setproducts] = useState([])
-<<<<<<< HEAD
-    const getProducts =[];
-    
-    useEffect(()=>{
-        const ShowAllProducts= async ()=>{
-
-            const docRef = collection(db, "Products");
-            let docSnap = await getDocs(docRef);
-            docSnap.forEach((doc)=>{
-                // console.log(doc.id,"=>",doc.data());
-                getProducts.push({
-                    key: doc.id,
-                    ...doc.data()
-                })
-                // console.log(getProducts);
-                setproducts(getProducts)
-            });
-            console.log(getProducts)
-        }    
-        ShowAllProducts()
-        return(
-            <div>
-                {getProducts.map((product)=> <div key={product.id}>
-                {product.title} <br/>
-                {product.description} <br/>
-                {product.price}<br/>
-                <img with="100px" height="100px" src={product.photoUrl}/>
-            </div>)
-                }
-            </div>
-        )
-=======
     const ShowAllProducts= async ()=>{
         const getProducts =[];
 
@@ -57,26 +25,14 @@ const ShowProducts=()=>{
     useEffect(()=>{
         ShowAllProducts()
        
->>>>>>> ec0cc4d029510387844d19cfd58972659f7dbbeb
     },[])
    
     return (
         <div className="">
             {/* <button className="btn btn-" onClick={ShowAllProducts}>click</button> */}
-<<<<<<< HEAD
-            {/* {products.map((product)=> <div key={product.id}>
-                {product.title} <br/>
-                {product.description} <br/>
-                {product.price}<br/>
-                <img with="100px" height="100px" src={product.photoUrl}/>
-            </div>)} */}
-            {/* <ShowAllProducts/> */}
-            {useEffect()}
-=======
             {products.map((product)=> 
             <Product props={product}/>)}
         
->>>>>>> ec0cc4d029510387844d19cfd58972659f7dbbeb
         </div>
     )
 }
