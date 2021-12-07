69551c4ef2a20fdf2e041825aee044f109441b51
Quiet0o <mikolajklosowski112@gmail.com>
2021-12-07 17:05:38 +0100

    test

--- src/componets/ProductComponets/SingleProduct.js
+++ src/componets/ProductComponets/SingleProduct.js
@@ -1,25 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 import  {getDoc,doc }  from "firebase/firestore"; 
 import { db } from '../confing/firebase-config';
-
+import ErrorPage from '../ErrorPage';
 import { useParams } from 'react-router';
 const SingleProduct =()=>{
     let {ProductId} = useParams();
     const getSingleProduct =[];
-    
+    const [err,setErr]= useState(false)
     const showOneProduct = async(docId) =>{
         const docRef = doc(db, "Products",docId);
-        let docSnap = await getDoc(docRef);
+        let docSnap = await getDoc(docRef).then(()=>{
+            getSingleProduct.push({key:docSnap.id, ...docSnap.data()})
+            
+        }).catch(()=>{
+            // <ErrorPage/>
+            setErr(!err)
+        });
         
-        getSingleProduct.push({key:docSnap.id, ...docSnap.data()})
+        // getSingleProduct.push({key:docSnap.id, ...docSnap.data()})
 
         // log.info(getSingleProduct)
 
         console.log(getSingleProduct)
     }
-    showOneProduct(ProductId)
+    // showOneProduct(ProductId)
     return (
-        <p>{ProductId}</p>
+        <div className="">
+               <p> {err.valueOf}</p>
+                <p>{ProductId}</p>
+            </div>
         )
 }
 export default SingleProduct;
