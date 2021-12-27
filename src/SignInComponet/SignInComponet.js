82fbec7dc54ae28507a12a6251e62135c4c175e4
Quiet0o <mikolajklosowski112@gmail.com>
2021-12-27 16:16:18 +0100

    dupa

--- src/SignInComponet/SignInComponet.js
+++ src/SignInComponet/SignInComponet.js
@@ -53,10 +53,31 @@ const SingInComponent = () => {
       console.log(user);
       CheckoutAdmin();
     } catch (err) {
+      console.log(err);
+      switch (err.code) {
+        case "auth/user-not-found":
+          alert("user not found!");
+          break;
+        case "auth/email-already-exists":
+          alert(" email is already in use by an existing user!");
+          break;
+        case "auth/invalid-email":
+          alert("email user property is invalid. It must be a string email address!");
+          break;
+          case "auth/invalid-password":
+            alert("The provided value for the password user property is invalid!");
+            break;
+            case "auth/wrong-password":
+              alert("The provided value for the password user property is invalid2!");
+              break;
+          case "auth/too-many-requests":
+                alert("Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later!");
+                break;
+      }
       console.log(err.message);
     }
   };
- 
+
   // const SingInWithGoogle = () => {
   //   signInWithPopup(auth, new GoogleAuthProvider())
   //     .then(() => {
@@ -130,7 +151,6 @@ const SingInComponent = () => {
           </button>
           <br />
           <br />
-         
         </div>
       )}
       {user ? <AdminPage /> : <p></p>}
